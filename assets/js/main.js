d2e57a85c84f0ae8d00a25c638aaf485934f37fc
dnsgk4983 <dnsgk4983@gmail.com>
2020-10-22 18:38:45 +0900

    Oasi
    Oasi

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,47 +125,8 @@
     return false;
   });
 
-  // jQuery counterUp
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
 
-  // Skills section
-  $('.skills-content').waypoint(function() {
-    $('.progress .progress-bar').each(function() {
-      $(this).css("width", $(this).attr("aria-valuenow") + '%');
-    });
-  }, {
-    offset: '80%'
-  });
 
-  // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-    });
-    // Initiate venobox (lightbox feature used in portofilo)
-    $(document).ready(function() {
-      $('.venobox').venobox();
-    });
-  });
-
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
 
   // review
   $(".testimonials-carousel").owlCarousel({
@@ -189,60 +150,6 @@
   });
 
 
-  // ROOMS slide
-  $(function(){
-    var $slider = $('.slider'),
-        $firstSlide = $slider.find('li').first() // 첫번째 슬라이드
-    .stop(true).animate({'opacity':1},200); // 첫번째 슬라이드만 보이게 하기
-  
-    function PrevSlide(){ // 이전버튼 함수
-      var $lastSlide = $slider.find('li').last() //마지막 슬라이드
-      .prependTo($slider); //마지막 슬라이드를 맨 앞으로 보내기  
-      $secondSlide = $slider.find('li').eq(1)
-     //두 번째 슬라이드 구하기
-     .stop(true).animate({'opacity':0},400); //밀려난 두 번째 슬라이드는 fadeOut 시키고
-      $firstSlide = $slider.find('li').first() //맨 처음 슬라이드 다시 구하기
-      .stop(true).animate({'opacity':1},400);
-      //새로 들어온 첫 번째 슬라이드는 fadeIn 시키기
-    }
-    function NextSlide(){ // 다음 버튼 함수
-      $firstSlide = $slider.find('li').first() // 첫 번째 슬라이드
-      .appendTo($slider); // 맨 마지막으로 보내기
-      var $lastSlide = $slider.find('li').last() // 맨 마지막으로 보낸 슬라이드
-      .stop(true).animate({'opacity':0},400); // fadeOut시키기
-      $firstSlide = $slider.find('li').first()
-      // 맨 처음 슬라이드
-      .stop(true).animate({'opacity':1},400);
-      // fadeIn 시키기
-    }
-    
-    $('#next').on('click', function(){ //다음버튼 클릭
-      NextSlide();
-    });
-      $('#prev').on('click', function(){ //이전 버튼 클릭하면
-      PrevSlide();
-    });
-    
-    setInterval(NextSlide, 3000); //자동 슬라이드 설정
-  
-  });
-
-  // 슬라이드2
-  $('#keyvisualpager li a').click(function () { 
-
-    // get position of a element
-    var mbr_index = $(this).parent().prevAll().length;
-    var mbr_targetkeyvisual = mbr_index + 1;
-
-    // hide current image and show the target image
-    $('#keyvisualslides li:visible').hide();        
-    $('#keyvisualslides li:nth-child('+mbr_targetkeyvisual+')').show()
-
-    // remove active class from current indicator and add the same class to target indicator
-    $('#keyvisualpager li a').removeClass('keyvisualactive');
-    $('#keyvisualpager li:nth-child('+mbr_targetkeyvisual+') a').addClass('keyvisualactive');
-
-});
   
 })(jQuery);
 
